def9f90f838054fa4716314369819be4531025db
Fabiano Góes <fabianogoes@gmail.com>
2015-08-05 14:10:22 -0300

    menu active control

--- src/main/webapp/resources/core/controllers/home-controller.js
+++ src/main/webapp/resources/core/controllers/home-controller.js
@@ -8,8 +8,11 @@ app.controller('HomeController', ['$http', '$location', '$window',
 		 * id_menu_aluno: seletor dentro do arquivo > home-top.jsp 
 		 */
 		$("#id_menu_aluno").removeClass( "active" );
+		$("#id_menu_professor").removeClass( "active" );
+		$("#id_menu_curso").removeClass( "active" );
 		$("#id_menu_sobre").removeClass( "active" );
 		$("#id_menu_home").addClass( "active" );
+		
 		$("#id_moduleDescription").html( "Sistema Integrado para Gestao de Aulas" );
 	}
 
