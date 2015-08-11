755187a5c920447e78991ea8ce18d90e0cfd3a57
Fabiano Góes <fabianogoes@gmail.com>
2015-08-11 20:43:37 -0300

    dsEnvironment in session context and show in footer view

--- src/main/java/com/eprogramar/sigaula/controllers/HomeController.java
+++ src/main/java/com/eprogramar/sigaula/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package com.eprogramar.sigaula.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +17,12 @@ import com.eprogramar.sigaula.repositories.ProfessorRepository;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired @Qualifier("dsEnvironment")
+	private String dsEnvironment;
+	
+	@Autowired @Qualifier("persistenceUnitEnvironment")
+	private String persistenceUnitEnvironment;
 
 	@Autowired
 	private AlunoRepository alunoRepository;
@@ -22,13 +31,15 @@ public class HomeController {
 	private ProfessorRepository professorRepository;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String root() {
+	public String root(HttpSession session) {
 		logger.info("root()...");
+		System.out.println( dsEnvironment );
+		session.setAttribute( "dsEnvironment" , dsEnvironment+" - "+persistenceUnitEnvironment );
 		return "home";
 	}
 	
 	@RequestMapping("/welcome")
-	public String welcome() {
+	public String welcome(HttpSession session) {
 		logger.info("home()...");
 		
 //		Aluno aluno = new Aluno();
@@ -46,6 +57,9 @@ public class HomeController {
 //		professor.setNome("Professor");
 //		this.professorRepository.save(professor);
 //		System.out.println( "Professor Saved = "+professor );
+		
+		System.out.println( dsEnvironment );
+		session.setAttribute( "dsEnvironment" , dsEnvironment );
 		
 		return "core/home-description";
 	}
--- src/main/webapp/WEB-INF/views/core/home-footer.jsp
+++ src/main/webapp/WEB-INF/views/core/home-footer.jsp
@@ -4,13 +4,14 @@
         	<a href="http://opensource.org/licenses/MIT" target="_blank">
 				<img class="pull-right" alt="MIT License" src="${ pageContext.request.contextPath }/resources/img/mit.png" height="30px">
         	</a>
+        	
         	<img alt="SIG" src="${ pageContext.request.contextPath }/resources/img/sig.png" height="30px">&nbsp;
         	<span class="badge"> ${ initParam.version } </span> |  
 			Development by <strong><a href="https://br.linkedin.com/in/fabianogoes" target="_blank">Fabiano Góes</a></strong> &nbsp;|&nbsp; 
 			<a href="http://www.eprogramar.com/" target="_blank">
 				<span class="glyphicon glyphicon-education" aria-hidden="true"></span> <strong>e-Programar</strong>
 			</a> 
-			at <span class="badge">2015</span>. 
+			at <span class="badge">2015</span>. &nbsp; | &nbsp; Environment: <span class="badge">${ dsEnvironment }</span>
         </p>
       </div>
     </footer>
